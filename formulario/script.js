053190804729c919c306c71ec23b4bca555ca875
charles busso <109078429+charlesbusso@users.noreply.github.com>
2022-09-23 10:00:36 -0300

    ok

--- formulario/script.js
+++ formulario/script.js
@@ -22,14 +22,38 @@ function checkInputs() {
         setSuccessFor(username);
     }
 
-    if (emailValue === '') {
-        setErrorFor(email, 'O email é obrigatório.');
-    } else if () {
-
-    } else {
+    if (emailValue === "") {
+        setErrorFor(email, "O email é obrigatório.");
+    }  else if (!checkEmail(emailValue)){
+        setErrorFor(email, "por favor, insira um email válido");
+      }  else {
         setSuccessFor(email);
     }
-    
+
+    if (passwordValue === "") {
+        setErrorFor(password, "A senha é obrigatória.");
+        } else if (passwordValue.length < 8) {
+            setErrorFor(password,"A senha precisa ter no mínimo  caracteres.");
+        } else {
+            setSuccessFor(password);
+        }
+
+        if (passwordConfirmationValue === "") {
+            setErrorFor(passwordConfirmation, "A confirmação da senha é obrigatório.");
+        } else if (passwordConfirmationValue != passwordValue) {
+            setErrorFor(passwordConfirmation, "As senhas não conferem.");
+        } else {
+            setSuccessFor(passwordConfirmation);
+    }
+    const formControls = form.querySelectorAll('.form-conrol')
+
+    const formIsvalid = [...formControls].every((formControl) => {
+        return (formControl.className === "form-control success");
+    });
+
+}
+
+
 
 
 function setErrorFor(input, message) {
@@ -48,7 +72,13 @@ function setSuccessFor(input){
 
     formControl.className = "form-control success";
     
+}
+function checkEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      );
+    
+}
     
 
-}
     
